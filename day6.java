dad1ed3aaedcf1a874c8b4deaa55eb0ab73ad59f
arpitguptaalm-art <arpitguptaalm1@gmail.com>
2026-01-23 22:05:36 +0530

    Day6

---
+++ day6.java
@@ -0,0 +1,19 @@
+// conversion of seconds into hours:minutes:seconds
+
+package javagenesis;
+import java.util.Scanner;
+public class day6 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner (System.in);
+        int sec=scan.nextInt();
+        int hrs=00,min=00;
+        if (sec>=0)
+        {
+            hrs=sec/3600;
+                min=(sec%3600)/60;
+                sec=(sec%60)%60;
+        }
+        System.out.printf("%d:%d:%d",hrs,min,sec);
+    }
+    }
+
