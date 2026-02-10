010aa84dfba7a1eb3efabc53cc72b92322b3089e
arpitguptaalm-art <arpitguptaalm1@gmail.com>
2026-02-10 23:45:11 +0530

    day 24

---
+++ day24.java
@@ -0,0 +1,31 @@
+// linear search
+
+package javagenesis;
+import java.util.Scanner;
+public class day24 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int key=sc.nextInt();
+        boolean found=false;
+        for (int i=0;i<n;i++)
+        {
+            if(arr[i]==key)
+            {
+                System.out.println("Element found at index: "+i);
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+        {
+            System.out.println("Element not found in the array");
+        }
+        sc.close();
+    }
+}
