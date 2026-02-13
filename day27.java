56007bd5a3fce9ad4a9e53b9cafbd93e5d09d246
arpitguptaalm-art <arpitguptaalm1@gmail.com>
2026-02-13 23:29:05 +0530

    day 27

---
+++ day27.java
@@ -0,0 +1,24 @@
+// array deletion
+
+package javagenesis;
+import java.util.*;
+public class day27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt(); 
+         int arr[] = new int[n];
+          for(int i=0;i<n;i++)
+            {
+                 arr[i] = sc.nextInt();
+                 }
+                  int pos = sc.nextInt(); 
+                   for(int i=pos-1;i<n-1;i++){ 
+                    arr[i] = arr[i+1];
+                 } 
+                  for(int i=0;i<n-1;i++)
+                    { System.out.print(arr[i]+" "); 
+
+                    }
+                    sc.close();
+    }
+}
