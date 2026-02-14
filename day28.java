cdfdbb09b4e74eb04425007b71b859fbb905da1b
arpitguptaalm-art <arpitguptaalm1@gmail.com>
2026-02-14 23:54:22 +0530

    day 28

---
+++ day28.java
@@ -0,0 +1,25 @@
+// pair sum in array
+
+package javagenesis;
+import java.util.*;
+public class day28 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt(); 
+         int arr[] = new int[n];
+          for(int i=0;i<n;i++)
+            {
+                 arr[i] = sc.nextInt();
+                 }
+                  int sum = sc.nextInt(); 
+                   for(int i=0;i<n;i++){ 
+                    for(int j=i+1;j<n;j++){
+                        if(arr[i]+arr[j]==sum){
+                            System.out.println(arr[i]+" "+arr[j]);
+                        }
+                    }
+                 } 
+                 sc.close();
+    }
+}
