9a981922fe9a68a434b0ad809296e4a93ae98501
arpitguptaalm-art <arpitguptaalm1@gmail.com>
2026-02-28 22:20:55 +0530

    day39

--- day38.java
+++ day38.java
@@ -32,3 +32,4 @@ public class day38 {
         scanner.close();    
     }
 }
+1
---
+++ day39.java
@@ -0,0 +1,27 @@
+//linear search
+
+
+import java.util.*;
+public class day39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+        boolean found = false;
+        for(int i=0; i<n; i++){
+            if(arr[i] == key){
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.println("Element not found");
+        }
+        sc.close();
+    }
+}
