590cca1e00b0cc8f7996494949d8fe75e352ab90
yashashvigupta16 <yashashvigupta16@gmail.com>
2026-03-22 14:57:33 +0530

    Update name in output message

--- day1.java
+++ day1.java
@@ -3,6 +3,6 @@ package javagenesis;
 public class day1 {
     public static void main(String[]ar)
     {
-        System.out.println("Hi my name is Arpit Gupta.");
+        System.out.println("Hi my name is Topper Arpit Gupta.");
     }
 }
